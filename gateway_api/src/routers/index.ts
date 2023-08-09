886fc8181787718e45832c635a86cce529b73d8e
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-08-09 12:59:57 +0700

    Gateway_API: Create documentation routes
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- gateway_api/src/routers/index.ts
+++ gateway_api/src/routers/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { apiRouter } from "./api";
+import { docsRouter } from "./doc";
 
 export const routers = Router();
 
 routers.use("/api/v1", apiRouter);
+routers.use("/api/v1/auth/docs", docsRouter);
